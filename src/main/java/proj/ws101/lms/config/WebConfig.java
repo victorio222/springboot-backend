f424ec50505ee2612154ed6ee0181a3c9d1e080c
cabatinganvictorio <cabatinganvictorio@gmail.com>
2024-12-21 12:59:59 +0800

    Initial commit

--- src/main/java/proj/ws101/lms/config/WebConfig.java
+++ src/main/java/proj/ws101/lms/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3001") // allow React app to access API
+                .allowedOrigins("https://musical-clafoutis-fa362b.netlify.app") // allow React app to access API
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true); // allow cookies/session headers to be sent
